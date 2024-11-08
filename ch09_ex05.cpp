119122d54adac2b2eeedb0287a4c28fbff0a390b
sejin1owo <yu03471aaa@gmail.com>
2024-11-08 09:17:21 +0900

    Create ch09_ex05.cpp

---
+++ ch09_ex05.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+union example { 
+int i;
+char c; 
+};
+
+int main(void) 
+{
+  union example v; 
+  v.c = 'A';
+  printf("v.c:%c v.i:%i\n", v.c, v.i ); 
+  v.i = 10000;
+  printf("v.c:%cv.i:%i\n", v.c, v.i);
+}
